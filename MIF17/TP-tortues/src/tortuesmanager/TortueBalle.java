c1d9b627d27f8a3139b4ae5d1a81aa4f125a5e91
lance.florian <lance.florian@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-09-18 15:17:38 +0000

    modification Tortue

--- MIF17/TP-tortues/src/tortuesmanager/TortueBalle.java
+++ MIF17/TP-tortues/src/tortuesmanager/TortueBalle.java
@@ -1,5 +1,52 @@
 package tortuesmanager;
 
-public class TortueBalle {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Polygon;
+
+
+
+public class TortueBalle extends Tortue {
+
+/**
+     * Constructeur pour une TortueBalle, aura un nom par defaut
+     * @param f La tortue sera rattache a cette feuille
+     */
+    public TortueBalle(FeuilleDessin f)
+    {
+        super(f);
+
+        setCouleur(Color.RED);
+    }
+
+    /**
+     * Dessine la TortueBalle dans la FeuilleDessin.<br/>
+     * On n'appellera pas directement cette fonction, le programme se charge
+     * de l'affichage des tortues
+     * @param g C'est le contexte graphique de la FeuilleDessin a laquelle
+     * la TortueBalle est rattache.
+     */
+    protected void drawTurtle(Graphics g)
+    {
+        Polygon circle = new Polygon();
+
+        //Rayon du cerle
+        double r = 5;
+
+        int n = 16;
+        for(int i=0; i<=n; i++)
+        {
+                Point p = new Point(x+ (int)(r * Math.cos(i*(2*Math.PI/n))),
+                                    y+ (int)(r * Math.sin(i*(2*Math.PI/n))));
+                circle.addPoint(p.x, p.y);
+        }
+
+        g.setColor(tortueCouleur);
+        g.fillPolygon(circle);
+
+        //g.drawArc(x, y, 2*rp, 2*rp, 0, 360);
+        //g.fillOval(x, y, 2*rp, 2*rp);
+    }
 
 }
