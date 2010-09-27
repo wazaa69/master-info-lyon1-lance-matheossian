ec26f0e8e3e0e2d108126676ef98586bb788cc07
lance.florian <lance.florian@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-09-27 19:57:49 +0000

    "ajout dossier mif16"

---
+++ MIF16/Doxygen/Présentation/sources/ClassTest1.cpp
@@ -0,0 +1,21 @@
+
+
+
+#include "ClassTest1.h"
+
+ClassTest1::ClassTest1()
+{
+
+}
+
+
+ClassTest1(int arg1)
+{
+  attribut1 = arg1;
+}
+
+
+ClassTest1::~ClassTest1()
+{
+
+}
---
+++ MIF16/Doxygen/Présentation/sources/ClassTest2.cpp
@@ -0,0 +1,11 @@
+#include "ClassTest2.h"
+
+ClassTest2::ClassTest2()
+{
+    //ctor
+}
+
+ClassTest2::~ClassTest2()
+{
+    //dtor
+}
