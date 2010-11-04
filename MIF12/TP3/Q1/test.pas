b99a6eb0cda208d2e2f6175393eb4b29d4497b1f
lance.florian <lance.florian@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-11-04 22:24:56 +0000

    cool

--- MIF12/TP3/Q1/test.pas
+++ MIF12/TP3/Q1/test.pas
@@ -1,9 +1,6 @@
 program test;
 
-const   cons1 = 10 - (1 * 10)+ 4;
-	cons2 = 'test' <> 'test';
-	cons3 = 'test' = 'test';
-
+const   con2 = true;
 type tarray = array [ 1.. 10 ] of integer;
 var v0 , v1 : integer;
     vreal,vreal2  : real;
@@ -17,17 +14,35 @@ var v0 , v1 : integer;
 		end;
     i1 : 1..10; 
 
-procedure remplirTab;
-begin
-   r11;
-   b1
-  
-end;
 
-
+procedure proc1 ( var dest : tarray );
+const   con3 = true;
+type tarray2 = array [ 1.. 10 ] of integer;
+var i1 : integer;
 begin
   r11;
   b1
-end.
+end;
 
+procedure proc2 ( var dest : tarray );
+const   con4 = true;
+type tarray3 = array [ 1.. 10 ] of integer;
+var i2 : integer;
+begin
+  r11;
+  b1
+end;
+
+procedure proc3 ( var dest : tarray );
+const   con5 = true;
+type tarray4 = array [ 1.. 10 ] of integer;
+var i3 : integer;
+begin
+  r11;
+  b1
+end;
+
+begin
+  b1
+end.
 
