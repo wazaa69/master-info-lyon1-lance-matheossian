9799a71fb8f912cc2b8a1eb238a425560db04bb9
anima876 <anima876@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-11-11 16:52:28 +0000

    WEB TP6 - début première partie

---
+++ MIF13/TP6/WebApplication/src/java/org/yournamehere/client/ClickBouton.java
@@ -0,0 +1,15 @@
+package org.yournamehere.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+
+/**
+ * Redéfinition du click d'un bouton
+ */
+public class ClickBouton implements ClickHandler {
+
+    public void onClick(ClickEvent event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
