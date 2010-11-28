8a69acae13cc75c8dd3ff36d695c76d0006ff742
anima876 <anima876@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-11-28 15:14:34 +0000

    Maj première partie Prolog

--- MIF14/TP3-4/exercice1.pl
+++ MIF14/TP3-4/exercice1.pl
@@ -1,24 +1,74 @@
-#Point de départ
-rive(1)
+/* Etats interdits sur Rive 0 */
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 1, Loup = 1, Chevre = 0, Chou = 0.
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 1, Loup = 0, Chevre = 0, Chou = 1.
 
-#Point d'arrivé
-rive(2)
-
-fermier(fermier).
-loup(loup).
-chevre(chevre)
-choux(choux).
+/* Etats interdits sur Rive 1 */
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 0, Loup = 1, Chevre = 1, Chou = 0.
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 0, Loup = 0, Chevre = 1, Chou = 1.
 
 
-#
-protagoniste
+/* Etats interdits non atteignable */
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 0, Loup = 1, Chevre = 1, Chou = 1.
+interdit(etat(Fermier, Loup, Chevre, Chou)) :- Fermier = 1, Loup = 0, Chevre = 0, Chou = 0.
 
-#Verification du type de la valeur en paramètre
-type_respect(Fermier, Loup, Chevre, Choux) :- fermier(Fermier), loup(Loup), chevre(Chevre), choux(Choux).
 
-#Définition des états interdits
-interdit(Fermier, Loup, Chevre, Choux) :- Fermier == 0, Loup == 1, Chevre == 1.
-interdit(Fermier, Loup, Chevre, Choux) :- Fermier == 0, Chevre == 1, Choux == 1.
+/* <----------------------------------------------------------------------------------->
+	Déclaration de ce qui est doit être vrai dans toutes les situations.
+	Création d'un nouvel Etat lors du Transport d'un protagoniste d'une Rive à une autre (si les interdits sont respectés).
+*/
+ 
+/* Transport Fermier + Chou */
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(0,0,Chevre,Loup)) :- Chou = 1, Fermier = 1, not(interdit(etat(0,0,Chevre,Loup))).
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(1,1,Chevre,Loup)) :- Chou = 0, Fermier = 0, not(interdit(etat(1,1,Chevre,Loup))).
 
-#
-peutPasser(Protagoniste, Rive) :- 
+/* Transport Fermier + Chèvre */
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(0,Chou,0,Loup)) :- Chevre = 1, Fermier = 1, not(interdit(etat(0,Chou,0,Loup))).
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(1,Chou,1,Loup)) :- Chevre = 0, Fermier = 0, not(interdit(etat(1,Chou,1,Loup))).
+
+/* Transport Fermier + Loup */
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(0,Chou,Chevre,0)) :- Loup = 1, Fermier = 1, not(interdit(etat(0,Chou,Chevre,0))).
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(1,Chou,Chevre,1)) :- Loup = 0, Fermier = 0, not(interdit(etat(1,Chou,Chevre,1))).
+
+/* Transport Fermier (exemple : chèvre de Rive 0 à Rive 1) */
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(0,Chou,Chevre,Loup)) :- Fermier = 1, not(interdit(etat(0,Chou,Chevre,Loup))).
+etatsuivant(etat(Fermier,Chou,Chevre,Loup), etat(1,Chou,Chevre,Loup)) :- Fermier = 0, not(interdit(etat(1,Chou,Chevre,Loup))).
+
+
+/* <----------------------------------------------------------------------------------->
+	recherche des opérations permettant de passer de l’état initial à l’état final
+*/
+
+resolution(EtatCourt, EtatSuivant, ListeEtatsSuccTmp, ListeEtatsSucc):- /* But atteint */
+	EtatCourt = EtatSuivant, /* EtatCourt = (1,1,1,1) et EtatSuivant = (1,1,1,1) */
+	append(ListeEtatsSuccTmp,[],ListeEtatsSucc).
+	
+resolution(EtatCourt, EtatBut, ListeEtatsSuccTmp, ListeEtatsSucc) :-
+	etatsuivant(EtatCourt, EtatSuivt),
+	not(member(EtatSuivt, ListeEtatsSuccTmp)),
+	resolution(EtatSuivt, EtatBut, [EtatSuivt | ListeEtatsSuccTmp], ListeEtatsSucc).
+
+
+/* [EtatInit] la liste des états sucessif, jusqu'à la résolution */
+transports(EtatInit, EtatBut, ListeEtatsSucc) :- resolution(EtatInit, EtatBut, [EtatInit], ListeEtatsSucc).
+
+
+/* <----------------------------------------------------------------------------------->
+	Lancement résolution et Affichage
+*/
+
+/* Affichage ligne par ligne */	
+afficherLigne(X,[]):- write(X), nl.
+afficherLigne(X, [T|Q]):- write(X), nl, afficherLigne(T,Q).
+afficher([T|Q]):- afficherLigne(T,Q).
+
+
+/* On fournit :
+	- l'état initial,
+	- le but,
+	- et un ordre de Transport (initiallemen vide)
+*/
+depart :-
+	transports(etat(0,0,0,0), etat(1,1,1,1), ListeEtatsSucc),
+	reverse(ListeEtatsSucc,R),
+	write('etat(F,Cu,Cr,L)'), nl, nl,
+	afficher(R).
