7af0c946b8fc15ce627f529b5b2785ffa8519cd5
anima876 <anima876@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-12-01 19:16:11 +0000

    Tp noté terminé, check faute orth par Flo

--- trunk/MIF14/TP3-4/exercice3.pl
+++ trunk/MIF14/TP3-4/exercice3.pl
@@ -1,6 +1,6 @@
 /* Contenance max des cruches */
-tailleCruche(1, 8).
-tailleCruche(2, 5).
+tailleCruche(1, 8). /* grande cruche = n°1 */
+tailleCruche(2, 5). /* petite cruche = n°2 */
 
 
 /* Etat initial et buts */
@@ -22,48 +22,51 @@ interdit(etat(_, Cruche2)) :- Cruche2 < 0.
 
 
 
-/* Remplir cruche 1 */
-etatsuivant(etat(_, Cruche2), r1, etat(8, Cruche2)) :- not(interdit(etat(8, Cruche2))).
+/* Remplir la grande cruche */
+etatsuivant(etat(_, Cruche2), 'r1 = remplir la cruche 1' , etat(8, Cruche2)) :- not(interdit(etat(8, Cruche2))).
 
-/* Remplir cruche 2 */
-etatsuivant(etat(Cruche1, _), r2, etat(Cruche1, 5)) :- not(interdit(etat(Cruche1, 5))).
+/* Remplir la petite cruche */
+etatsuivant(etat(Cruche1, _), 'r2 = remplir la cruche 2', etat(Cruche1, 5)) :- not(interdit(etat(Cruche1, 5))).
 
-/* Vider cruche 1 */
-etatsuivant(etat(_, Cruche2), r3, etat(0, Cruche2)) :- not(interdit(etat(0, Cruche2))).
+/* Vider la grande cruche */
+etatsuivant(etat(_, Cruche2), 'r3 = vider la cruche 1', etat(0, Cruche2)) :- not(interdit(etat(0, Cruche2))).
 
-/* Vider cruche 2 */
-etatsuivant(etat(Cruche1, _), r4, etat(Cruche1, 0)) :- not(interdit(etat(Cruche1, 0))).
+/* Vider la petite cruche */
+etatsuivant(etat(Cruche1, _), 'r4 = vider la cruche 2', etat(Cruche1, 0)) :- not(interdit(etat(Cruche1, 0))).
 
 
 
-/* On vide cruche 1 dans cruche 2 */
-etatsuivant(etat(Cruche1, Cruche2), r5, etat(CrucheTmp1, T2)) :-
-    tailleCruche(2, T2), /* on récupère la taille de la cruche 2 */
-    Max2 is T2 - Cruche2, /* Max2 = place restante dans la cruche 2 */
-    Cruche1 >= Max2, /* si la place restante est plus petite que le contenu de la cruche 1*/
-    CrucheTmp1 is Cruche1 - Max2,  
-    not(interdit(etat(CrucheTmp1, T2))).
-	
-etatsuivant(etat(Cruche1, Cruche2), r6, etat(0, CrucheTmp2)) :-
+/* On vide la grande cruche (n°1) dans la petite cruche (n°2) -------------------------------------> */
+
+/* la grande cruche se vide dans la petite, la petite cruche est pleine mais il en reste dans la grande */
+etatsuivant(etat(Cruche1, Cruche2), 'r5 = vider la cruche 1 dans la cruche 2 => la cruche 2 est pleine,  la cruche 1 est non vide', etat(CrucheTmp1, T2)) :-
     tailleCruche(2, T2),
-    Max2 is T2 - Cruche2,
-    Cruche1 < Max2,
+    Diff2 is T2 - Cruche2,
+    Cruche1 >= Diff2,
+    CrucheTmp1 is Cruche1 - Diff2,  
+    not(interdit(etat(CrucheTmp1, T2))).
+
+/* la grande cruche se vide dans la petite, la petite cruche est pleine et la grande est vide */
+etatsuivant(etat(Cruche1, Cruche2), 'r6 = vider la cruche 1 dans la cruche 2 => la cruche 2 est pleine, la cruche 1 est vide', etat(0, CrucheTmp2)) :-
+    tailleCruche(2, T2),
+    Diff2 is T2 - Cruche2,
+    Cruche1 < Diff2,
     CrucheTmp2 is Cruche1 + Cruche2,
     not(interdit(etat(0, CrucheTmp2))).
 
 	
-/* On vide cruche 2 dans cruche 1 */
-etatsuivant(etat(Cruche1, Cruche2), r7, etat(T1, CrucheTmp2)) :-
+/* On vide la petite cruche dans la grande cruche (inverse des précédants) -------------------------------------> */
+etatsuivant(etat(Cruche1, Cruche2), 'r7 = vider la cruche 2 dans la cruche 1 => la cruche 1 est pleine,  la cruche 2 est non vide', etat(T1, CrucheTmp2)) :-
     tailleCruche(1, T1),
-    Max1 is T1 - Cruche1, /* transvaser une partie de la cruche 2 dans la cruche 1 => comparaisons sur leurs contenus */
-    Cruche2 >= Max1,
-    CrucheTmp2 is Cruche2 - Max1,
+    Diff1 is T1 - Cruche1,
+    Cruche2 >= Diff1,
+    CrucheTmp2 is Cruche2 - Diff1,
     not(interdit(etat(T1, CrucheTmp2))).
 	
-etatsuivant(etat(Cruche1, Cruche2), r8, etat(CrucheTmp1, 0)) :-
+etatsuivant(etat(Cruche1, Cruche2), 'r8 = vider la cruche 2 dans la cruche 1 => la cruche 1 est pleine, la cruche 2 est vide', etat(CrucheTmp1, 0)) :-
     tailleCruche(1, T1),
-    Max1 is T1 - Cruche1,
-    Cruche2 < Max1,
+    Diff1 is T1 - Cruche1,
+    Cruche2 < Diff1,
     CrucheTmp1 is Cruche1 + Cruche2,
     not(interdit(etat(CrucheTmp1, 0))).
 
@@ -88,6 +91,6 @@ afficherLigne(X, [T|Q]):- write(X), nl, afficherLigne(T,Q).
 afficher([T|Q]):- afficherLigne(T,Q).
 
 
-/* On résoud le programme */
-resolution(Ei, ListeEtatsSucc, ListeOperSucc) :- rechPf(Ei, [Ei], []).
-resoudre :- initial(Ei), resolution(Ei, ListeEtatsSucc, ListeOperSucc).
+/* On résoud le problème */
+resolution(Ei) :- rechPf(Ei, [Ei], []).
+resoudre :- initial(Ei), resolution(Ei).
