d3273f29fc0dc2c4141dfe788b4cf72936b53f25
anima876 <anima876@d703d8df-48fa-1396-3bd6-e846a18e1d41>
2010-12-26 12:58:12 +0000

    Maj Mif16

---
+++ trunk/MIF16/Exposes/Doxygen (le notre)/Autres Doxygen/[MIF16] - TD2 Doxygen - Gr2 GAUDE-GUILLON/[MIF16] - TD1 Doxygen - Gr2 GAUDE-GUILLON/sources/ClassA.cpp
@@ -0,0 +1,12 @@
+/**
+ * Classe d'exemple A
+ */
+
+#include "ClassA.hpp"
+ 
+
+ClassA::ClassA();
+ClassA::~ClassA();
+double ClassA::maMethode();
+double ClassA::maMethode(int param1, const string param2);
+double ClassA::monAutreMethode(int param1 = 0);
